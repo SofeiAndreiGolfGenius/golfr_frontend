083c74172f05616ecffbc43e4d10108da32bfc4c
SofeiAndreiGolfGenius <andrei.sofei@golfgenius.com>
2022-08-02 21:41:58 +0300

    Fixed indentation again

--- pages/golfers/[id].js
+++ pages/golfers/[id].js
@@ -1,34 +1,34 @@
-import useUserInfo from "../../lib/useUserInfo";
-import ScoreCard from "../../components/ScoreCard";
-import { useRouter } from "next/dist/client/router";
-import Layout from "../../components/Layout";
+import useUserInfo from '../../lib/useUserInfo';
+import ScoreCard from '../../components/ScoreCard';
+import { useRouter } from 'next/dist/client/router';
+import Layout from '../../components/Layout';
 const User = () => {
   const route = useRouter()
   const golferId = route.query.id
   const { name, scores, error1, error2 } = useUserInfo(golferId)
-  return(
+  return (
     <Layout>
       <>
         {error1 ? (
           error1
-        ) : 
-        (error2 ? (
-          error2
         ) :
-          <>
-          <div>{ name }</div>
-            {scores && scores.map(score => (
-              <ScoreCard
-              key={score.id}
-              id={score.id}
-              totalScore={score.total_score}
-              playedAt={score.played_at}
-              userId={score.user_id}
-              userName={name}
-            />
-            ))}
-          </>
-        )}
+          (error2 ? (
+            error2
+          ) :
+            <>
+              <div>{ name }</div>
+              {scores && scores.map(score => (
+                <ScoreCard
+                key={score.id}
+                id={score.id}
+                totalScore={score.total_score}
+                playedAt={score.played_at}
+                userId={score.user_id}
+                userName={name}
+              />
+              ))}
+            </>
+          )}
       </>
     </Layout>
   )
